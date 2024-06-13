b5ba75d9b0f187f2eccccbce5236cbd9e19e19b2
unlimish <14168376+unlimish@users.noreply.github.com>
2024-06-13 13:31:58 +0900

    Add androidx.core.splashscreen dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,9 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation("androidx.core:core-splashscreen:1.0.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
 }
